de60252bd6a5f5d6f5788b771f406eb792276e1e
dawid <dawid@indemi.pl>
2023-07-05 20:41:07 +0200

    Initial commit - docker compose

--- src/Contract/TrafficSplitInterface.php
+++ src/Contract/TrafficSplitInterface.php
@@ -3,13 +3,6 @@
 namespace VoltDeveloperTask\Contract;
 use VoltDeveloperTask\Model\Payment;
 
-/**
- * Each class should have 2 public methods.
-* 1. Constructor - accepts one argument - list of possible payment gateways with preferred
-* weights.
-* 2. handlePayment(Payment $payment) - will route payment according to the weights passed
-* in construction
- */
 interface TrafficSplitInterface
 {
     /** @param PaymentGatewayInterface[] $paymentGateways */
--- src/Model/Payment.php
+++ src/Model/Payment.php
@@ -4,5 +4,22 @@ namespace VoltDeveloperTask\Model;
 
 class Payment
 {
+    private int $id;
+    private int $amount;
 
+    public function __construct(int $id, int $amount)
+    {
+        $this->id = $id;
+        $this->amount = $amount;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
 }
